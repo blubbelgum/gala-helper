b5f5197840fa3b8270e2fe95f15814bbf50c09d4
yeelan <43752564+blubbelgum@users.noreply.github.com>
2024-10-30 07:53:10 +0700

    Update smart_ai.js
    increase depth to 6

--- js/smart_ai.js
+++ js/smart_ai.js
@@ -49,8 +49,8 @@ SmartAI = function(game) {
 };
 
 SmartAI.prototype.nextMove = function() {
-  // Uses depth-5 search for move evaluation
-  return this.chooseBestMove2(this.game.grid, 5);
+  // Uses depth-6 search for move evaluation
+  return this.chooseBestMove2(this.game.grid, 6);
 };
 
 SmartAI.prototype.chooseBestMove2 = function(grid, numMoves) {
